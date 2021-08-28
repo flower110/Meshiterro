2ce5f89f32fce735f39e528cd9fbe049dc669a09
flower110 <yukait11@gmail.com>
2021-08-28 14:44:54 +0000

    ユーザー詳細ページ・プロフィール編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #get 'users/show'
   devise_for :users
   #devise_for :installs
   root to: 'homes#top'
@@ -6,5 +7,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show, :edit, :update]
 
 end
